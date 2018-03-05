1bb370c93d9cca84f95855a490345eba3bbce8a3
Taylor Donlan <taylordonlan@gmail.com>
2018-03-05 17:58:05 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
 # Add  code here!
+def prime?(number)
+  if number == 1
+    false
+  elsif number == 2
+    true
+  else
+    for i in 2...number do
+      return false if num % i == 0
+    end
+    return true
+  end
+  
