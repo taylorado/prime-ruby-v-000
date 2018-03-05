de1bdc00bb4148807a2d8765ea2076d52aba3b0b
Taylor Donlan <taylordonlan@gmail.com>
2018-03-05 18:04:08 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -2,12 +2,10 @@
 def prime?(number)
   return false if number <= 1
 
-#  elsif number == 2
-#    return true
-#  else
-    for i in 2...number do
-      return false if number % i == 0
-    end
-    return true
-#  end
+  for i in 2...number do
+    return false if number % i == 0
+  end
+
+  return true
+
 end
